72283e356833e04ca7d930ee126498f328c9423d
ZalinskyN24 <92743650+ZalinskyN24@users.noreply.github.com>
2024-11-12 19:41:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-<script>
-    document.getElementById('coletaDadosForm').addEventListener('submit', async function (e) {
-        e.preventDefault();
-        
-        const formData = new FormData(this);
+document.getElementById('coletaDadosForm').addEventListener('submit', async function (e) {
+    e.preventDefault();
+    
+    const formData = new FormData(this);
 
-        try {
-            const response = await fetch('URL_GOOGLE_APPS_SCRIPT', {
-                method: 'POST',
-                body: formData
-            });
-            if (response.ok) {
-                alert('Dados enviados com sucesso!');
-            } else {
-                alert('Erro ao enviar os dados');
-            }
-        } catch (error) {
-            console.error('Erro:', error);
+    try {
+        const response = await fetch('http://localhost:3000/enviar-dados', { // Altere para a URL do seu servidor
+            method: 'POST',
+            body: formData
+        });
+        if (response.ok) {
+            alert('Dados enviados com sucesso!');
+        } else {
+            alert('Erro ao enviar os dados');
         }
-    });
-</script>
+    } catch (error) {
+        console.error('Erro:', error);
+    }
+});
